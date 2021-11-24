0e2346626a6bb2316874a08660923db07975b458
Dr Natasha Hurley-Walker <nhw@icrar.org>
2021-11-24 14:04:36 +0800

    Rejigged path and added S/N filter

--- example_images/count_sources.sh
+++ example_images/count_sources.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-catalogue=../noise_analysis/XG_170-231MHz_psf_comp.fits
+catalogue=../alpha_distribution/IDR_v1.1_joined_rescaled_cat_seds_subset.fits
 
 #157.5-(3.5/2)
 #155.75000000000000000000
@@ -20,8 +20,8 @@ dec2=-29.25
 
 stilts tpipe \
         in=$catalogue \
-        cmd="select ((ra<$ra2)&&(ra>$ra1)&&(dec<$dec1)&&(dec>$dec2))" \
-        cmd='keepcols "ra dec"' \
+        cmd="select ((ref_ra<=$ra2)&&(ref_ra>=$ra1)&&(ref_dec<=$dec1)&&(ref_dec>=$dec2)&&(int_flux/local_rms>=5))" \
+        cmd='keepcols "ref_ra ref_dec"' \
         out='./number_sources.csv'
 
 n=`wc -l number_sources.csv | awk '{print $1}'`
